dd708661728669a7d2f5b9952ab82014ca5acbb6
Tarek Behairy <tbtarek@gmail.com>
2021-05-03 23:54:07 -0400

    db update

--- public/db.js
+++ public/db.js
@@ -25,3 +25,24 @@ function saveRecord(record) {
 
   store.add(record);
 }
+
+unction checkDatabase() {
+
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
